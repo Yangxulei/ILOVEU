2fdd41fb0a4066a4e34a52e9c84b95de159949ad
yxl <yangxulei84@qq.com>
2018-02-14 11:06:32 +0800

    init

---
+++ Vindicate/index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE HTML>
+<!--[if IE 6]>
+<html id="ie6" class="ie ie6 lt-ie9">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" class="ie ie7 lt-ie9">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" class="ie ie8 lt-ie9">
+<![endif]-->
+<!--[if gte IE 9]>
+<html class="ie ie9">
+<![endif]-->
+<!--[if !(IE)]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<meta http-equiv='Pragma' content='no-cache' />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+	<div class="flowtime">
+		<div class="ft-section section-1" data-id="section-1">
+			<div id="/section-1/page-1" class="ft-page page-1" data-id="page-1">
+				<p class="text1"><span id="text-75" contenteditable="false">你好哇，我不是张艺兴lay的Rai啊</span> </p>
+				<p>
+                    有一丽人,
+                    她生于河东，
+                    她心比天高，
+                    她命比风疾。
+                    曾悲思无穷而后憾，
+                    曾笑叹人生而自伤，
+                    曾漫步逍遥而自虞，
+                    曾求天得意而相亲。
+                    她形如钢、本性如水；
+                    口如利剑、心如浮萍。
+                    求天地风云而无所依，
+                    望日月星辰而无所求。
+                    形影相单，无所依从。<br>
+                    <br>
+                    有一美人在河之东，
+                    若问此人为谁：
+                    她曾望神思语，
+                    她朝喜暮悲。
+                    她来时响罗衣而不进，
+                    隐明灯而未前，
+                    步中檐而一思，
+                    顺长廊而回归。
+                    她去留余腻，沾妆委露；
+                    理鬓清癯，落花入领。
+                    曾因往昔黯然销魂，
+                    曾因七夕而心于独往。<br>
+                    若问此人为谁，
+                    乃河东月红也。<br>------------------
+
+                </p>
+曾记得这首词，你我相识快6年了。缘分是种很奇妙的东西，生活林林总总，该遇见的总会遇到！
+                <p class="text2">按键盘 "↓" <span id="text-78" contenteditable="true">听我说喽</span></p>
+			</div>
+
+			<div id="/section-1/page-2" class="ft-page page-2" data-id="page-2">
+				<p class="top-text" id="text-1" contenteditable="false">过去的我们一直是一个人生活，奋斗却留有遗憾，背负很多却不曾为自己。</p>
+				<img src='img/iali63.jpg'  alt="过去的我们一直是一个人生活，奋斗却留有遗憾，背负很多却不曾为自己。" />
+			</div>
+			<div id="/section-1/page-3" class="ft-page page-3 left-img" data-id="page-3">
+				<h2 class="text" id="text-2" contenteditable="false">一个人的长廊,自己走，自己承受</h2>
+				<img src='img/iali35.jpg'  alt="一个人的长廊,自己走，自己承受"/>
+			</div>
+			<div id="/section-1/page-4" class="ft-page page-4 full-img" data-id="page-4">
+				<h2 class="center-text" id="text-3" contenteditable="false">一个人的山岗，寂寞与谁表达</h2>
+				<img src='img/iali6.jpg'  alt="一个人的山岗，寂寞与谁表达" />
+			</div>
+		</div>
+		<div class="ft-section section-2" data-id="section-2">
+			<div id="/section-2/page-1" class="ft-page page-5 full-img" data-id="page-1">
+				<h2 class="center-text" id="text-4" contenteditable="false">一个人的地铁,失落又与谁倾诉</h2>
+				<img src='img/iali19.jpg'  alt="一个人的地铁,失落又与谁倾诉" />
+			</div>
+			<div id="/section-2/page-2" class="ft-page page-6" data-id="page-2">
+				<h2 class="center-text" id="text-5" contenteditable="false">一个人的游乐场，快乐又与谁分享</h2>
+				<img src='img/iali29.jpg' alt="一个人的游乐场，快乐又与谁分享"/>
+			</div>
+			<div id="/section-2/page-3" class="ft-page page-7" data-id="page-3">
+				<h3 id="text-6" contenteditable="false">依然乐观，微笑着，等待着背后又是什么滋味</h3>
+				<img src='img/iali60.jpg'  alt="依然乐观，微笑着，等待着背后又是什么滋味"/>
+			</div>
+			<div id="/section-2/page-4" class="ft-page page-8 full-img" data-id="page-4">
+				<h3 id="text-7" contenteditable="false">生活难免有风风雨雨，我们也永远不是最好的状态，没有影响他人之说</h3>
+				<img src='img/iali51.jpg' alt="生活难免有风风雨雨，我们也永远不是最好的状态，没有影响他人之说" />
+			</div>
+			<div id="/section-2/page-5" class="ft-page page-9" data-id="page-5">
+				<h3 id="text-8" contenteditable="false">一个人轻松的应对背后是多少的付出</h3>
+				<img src='img/iali5.jpg' />
+			</div>
+			<div id="/section-2/page-6" class="ft-page page-10" data-id="page-6">
+				<h3 id="text-9" contenteditable="false">面带阳光、自信的笑容是什么时候的事</h3>
+				<img src='img/iali22.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-3" data-id="section-3">
+			<div id="/section-3/page-1" class="ft-page page-11 full-img" data-id="page-1">
+				<h3 id="text-10" contenteditable="false">生活也不会总是一帆风顺</h3>
+				<img src='img/iali24.jpg'  />
+			</div>
+			<div id="/section-3/page-2" class="ft-page page-12" data-id="page-2">
+				<h3><span id="text-11" contenteditable="false">他愿站在某个人身前，每次都勇敢地为她抵挡风雨</span><br /><span id="text-12" contenteditable="false">随时准备接受生活的挑战</span></h3>
+				<img src='img/iali25.jpg' />
+			</div>
+			<div id="/section-3/page-3" class="ft-page page-13" data-id="page-3">
+				<img src='img/iali64.jpg' alt='可是Rai的爱情又在哪里呢？' />
+				<h3 id="text-13" contenteditable="false">可是Rai的爱情又在哪里呢？</h3>
+			</div>
+			<div id="/section-3/page-4" class="ft-page page-14 left-img" data-id="page-4">
+				<h3 id="text-14" contenteditable="false">在镜子里面吗？他不敢相信</h3>
+				<img src='img/iali46.jpg' />
+			</div>
+			<div id="/section-3/page-5" class="ft-page page-15 left-img" data-id="page-5">
+				<h3><span id="text-15" contenteditable="false">他去问大树，我的爱情在哪里？</span><br /><span id="text-16" contenteditable="false">大树告诉他，也许就在不远的地方</span></h3>
+				<img src='img/iali45.jpg' />
+			</div>
+			<div id="/section-3/page-6" class="ft-page page-16 left-img" data-id="page-6">
+				<h3><span id="text-17" contenteditable="false">于是，他一个人继续向前走</span><br /><span id="text-18" contenteditable="true">走在寒冷的冬日里</span></h3>
+				<img src='img/iali66.jpg' />
+			</div>
+			<div id="/section-3/page-7" class="ft-page page-17 top-text" data-id="page-7">
+				<h3 id="text-19" contenteditable="true">直到有一天Rai提前回家遇到了那个短发英姿飒爽的她</h3>
+				<img src='img/iali16.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-4" data-id="section-4">
+			<div id="/section-4/page-1" class="ft-page page-18 full-img" data-id="page-1">
+				<p class="text" id="text-20" contenteditable="true">Rai很久之前就懵懵懂懂喜欢英子，因为她的出现，<br>Rai脸上有了更加乐观开心的态度,<br></p>
+				<img src='img/iali11.jpg'  />
+			</div>
+			<div id="/section-4/page-2" class="ft-page page-19" data-id="page-2">
+				<h3 id="text-21" contenteditable="true">可是现实总有那么多纠结与不合时宜？</h3>
+				<img src="img/iali75.jpg" />
+			</div>
+			<div id="/section-4/page-3" class="ft-page page-20" data-id="page-3">
+				<img src='img/iali59.png' />
+				<h3 id="text-22" contenteditable="true">终于有一天，月全食之日，意外地点破</h3>
+				<img src='img/iali9.jpg' />
+			</div>
+			<div id="/section-4/page-4" class="ft-page page-21" data-id="page-4">
+				<h3 id="text-23" contenteditable="true">但是Rai却每天都很幸福和释然， 他要成为更好的自己</h3>
+				<img src='img/iali59.png' />
+			</div>
+			<div id="/section-4/page-5" class="ft-page page-22 left-img" data-id="page-5">
+				<h3 id="text-24" contenteditable="true">每天Rai都会去找英子聊天</h3>
+				<img src='img/iali4.jpg' />
+			</div>
+			<div id="/section-4/page-6" class="ft-page page-23 left-img" data-id="page-6">
+				<h3 id="text-25" contenteditable="false">希望两个人一起出去玩，送她去驾校，但是年前的Rai总是那么忙</h3>
+				<img src='img/iali44.jpg' />
+			</div>
+			<div id="/section-4/page-7" class="ft-page page-24 full-img" data-id="page-7">	
+				<h3 id="text-26" contenteditable="false">水库边的英子那么让人疼惜，Rai却只能心里暗暗发誓，<br>之前的生活没能参与，余生要让以后的英子永远开心快乐</h3>
+				<img src='img/iali32.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-5" data-id="section-5">
+			<div id="/section-5/page-1" class="ft-page page-25 left-img" data-id="page-1">
+				<h3><span id="text-27" contenteditable="false">Rai忙碌到很晚</span><br /><span id="text-28" contenteditable="false">收到一条关心的微信，<br>一天的疲劳都不翼而飞</span></h3>
+				<img src='img/iali37.jpg' />
+			</div>
+			<div id="/section-5/page-2" class="ft-page page-26 top-text" data-id="page-2">
+				<h3 id="text-29" contenteditable="false">然后高兴地进入梦乡，希望梦到英子</h3>
+				<img src='img/iali10.gif' />
+			</div>
+			<div id="/section-5/page-3" class="ft-page page-27" data-id="page-3">
+				<h3 id="text-30" contenteditable="false">梦中Rai很快就成为了英子的逛街助手</h3>
+				<img src='img/iali67.jpg'  />
+			</div>
+			<div id="/section-5/page-4" class="ft-page page-28" data-id="page-4">
+				<h3 id="text-31" contenteditable="false">Rai和英子一起去了好多地方玩</h3>
+				<img src='img/iali40.jpg' />
+			</div>
+			<div id="/section-5/page-5" class="ft-page page-29" data-id="page-5">
+				<h3 id="text-32" contenteditable="false">Rai也和英子一样成为了一个吃货</h3>
+				<img src='img/iali12.jpg' />
+			</div>
+			<div id="/section-5/page-6" class="ft-page page-30" data-id="page-6">
+				<h3 id="text-33" contenteditable="false">后来，他们可以天天相见</h3>
+				<img src='img/iali76.gif' />
+			</div>
+			<div id="/section-5/page-7" class="ft-page page-31" data-id="page-7">
+				<h3 id="text-34" contenteditable="false">于是Rai开始学习新的技能</h3>
+				<img src='img/iali77.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-6" data-id="section-6">
+			<div id="/section-6/page-1" class="ft-page page-32" data-id="page-1">
+				<h3 id="text-35" contenteditable="false">有空都给英子煮饭，弹吉他，一起读书，跑步</h3>
+				<img src='img/iali62.jpg'  />
+			</div>
+			<div id="/section-6/page-2" class="ft-page page-33 top-text" data-id="page-2">
+				<h3 id="text-36" contenteditable="false">他们吃着自己做的美食</h3>
+				<img src='img/iali3.jpg' />
+			</div>
+			<div id="/section-6/page-3" class="ft-page page-34 left-img" data-id="page-3">
+				<h3 id="text-37" contenteditable="false">然后在同一个站台，高高兴兴的一起上班/上学</h3>
+			</div>
+			<div id="/section-6/page-4" class="ft-page page-35" data-id="page-4">
+				<h3><span id="text-38" contenteditable="false">Rai会感觉自己好幸福。</span><br /><span id="text-39" contenteditable="false">因为有了港湾</span><br /><span id="text-40" contenteditable="false">那个有英子在的地方</span></h3>
+				<img src='img/iali8.gif' />
+			</div>
+			<div id="/section-6/page-5" class="ft-page page-36 full-img" data-id="page-5">
+				<h3 id="text-41" contenteditable="false">也许不会吵架,Rai说过女朋友都是用来宠的</h3>
+				<img src='img/iali31.jpg' />
+			</div>
+			<div id="/section-6/page-6" class="ft-page page-37 left-img" data-id="page-6">
+				<h3 id="text-42" contenteditable="false">如果有不开心的</h3>
+				<img src='img/iali49.jpg' />
+			</div>
+			<div id="/section-6/page-7" class="ft-page page-38 bottom-text" data-id="page-7">
+				<img src='img/iali21.jpg' />
+				<h3 id="text-43" contenteditable="false">一定是他自己有什么做的不对</h3>
+			</div>
+		</div>
+		<div class="ft-section section-7" data-id="section-7">
+			<div id="/section-7/page-1" class="ft-page page-39 left-img" data-id="page-1">
+				<h3 id="text-44" contenteditable="false">如果没有英子在身边，窗外就没有风景</h3>
+				<img src='img/iali2.jpg' />
+			</div>
+			<div id="/section-7/page-2" class="ft-page page-40" data-id="page-2">
+				<h3 id="text-45" contenteditable="false">如果没有英子在身后</h3>
+				<img src='img/iali14.jpg'  />
+			</div>
+			<div id="/section-7/page-3" class="ft-page page-41" data-id="page-3">
+				<img src='img/iali30_1.jpg' class='img1'/>
+				<img src='img/iali30_2.jpg' class='img2'/>
+				<h3 id="text-46" contenteditable="false"> Rai怎会更加激励自己奋斗成为更优秀的自己,<br>你说优秀的人总是会相互吸引</h3>
+			</div>
+			<div id="/section-7/page-4" class="ft-page page-42" data-id="page-4">
+				<h3><span id="text-47" contenteditable="false">美好的爱情不是卿卿我我，而是双方彼此成全自己</span><br /><span id="text-48" contenteditable="false">Rai许下的愿望就是让英子有个强大的依靠</span></h3>
+				<img src='img/iali69.jpg'  />
+			</div>
+			<div id="/section-7/page-5" class="ft-page page-43" data-id="page-5">
+				<img src='img/iali42.jpg' />
+				<h3 id="text-49" contenteditable="true">我愿为你变成那个驾着七彩祥云的英雄</h3>
+			</div>
+			<div id="/section-7/page-6" class="ft-page page-44 left-img" data-id="page-6">
+				<h3><span id="text-50" contenteditable="false">爱情就像花草一样</span><br /><span id="text-51" contenteditable="false">需要用包容和耐心来浇灌</span></h3>
+				<img src='img/iali0.jpg' />
+			</div>
+			<div id="/section-7/page-7" class="ft-page page-45 left-img" data-id="page-7">
+				<h3><span id="text-52" contenteditable="true">再到后来的每个节日</span><br /><span id="text-53" contenteditable="false">Rai会准备礼物</span></h3>
+				<img src='img/iali18.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-8" data-id="section-8">
+			<div id="/section-8/page-1" class="ft-page page-46" data-id="page-1">
+				<img src='img/iali57.gif' />
+				<h3>
+					<span id="text-54" contenteditable="true">看着礼物一件件准备好了</span>
+					<span id="text-55" contenteditable="true">Rai也会很开心</span>
+					<span id="text-56" contenteditable="true">因为他想象着英子收到礼物时</span>
+					<span id="text-57" contenteditable="true">开心的样子</span>
+				</h3>
+			</div>
+			<div id="/section-8/page-2" class="ft-page page-47 top-text" data-id="page-2">
+				<img src='img/iali58.jpg' />
+				<h3>
+					<span id="text-58" contenteditable="true">想想后面还有一辈子</span>
+					<span id="text-59" contenteditable="true">Rai好开心</span>
+					<span id="text-60" contenteditable="true">因为可以和英子</span>
+					<span id="text-61" contenteditable="true">去全世界好多地方玩</span></h3>
+				
+			</div>
+			<div id="/section-8/page-3" class="ft-page page-48 left-img" data-id="page-3">
+				<p><span id="text-62" contenteditable="true">Rai也会继续努力</span><br /><span id="text-63" contenteditable="false">为了以后的梦之城堡</span></p>
+				<img src='img/iali65.jpg'  />
+			</div>
+			<div id="/section-8/page-4" class="ft-page page-49 full-img" data-id="page-4">
+
+				<p class="text">
+                    Rai这段时间很幸福。<br />他相信后面的每天都会是快乐的，<br>
+                    简简单单<br />体会着与英子在一起的每一刻
+					<span class='text' id="text-64" contenteditable="false"></span>
+					<span class='text' id="text-65" contenteditable="false"></span>
+				</p>
+                <img src='img/iali71.jpg' />
+			</div>
+			<div id="/section-8/page-5" class="ft-page page-50" data-id="page-5">
+				<p class='text' id="text-66" contenteditable="true">后来的一天，可能英子说她不敢坐山车了，于是Rai就带她到别的地方玩。这只是他们幸福生活中的一件小事，小到无法引起别人的注意。可这是属于他的幸福生活。</p>
+				<img src='img/iali50_1.jpg' class='img1' />
+				<img src='img/iali50_2.jpg' class='img2' />
+			</div>
+			<div id="/section-8/page-6" class="ft-page page-51" data-id="page-6">
+				<p>
+					<span id="text-67" contenteditable="true">你若问：永远有多远？</span><br />
+					<span id="text-68" contenteditable="true">我会答：比时间多一秒就是永远，我会永远爱你</span><br />
+					<span id="text-69" contenteditable="true">你若问：世界有多大？</span><br />
+					<span id="text-70" contenteditable="true">我会答：你走到哪里，世界就有多大</span>
+					</p>
+			</div>
+		</div>
+		<div class="ft-section section-9" data-id="section-9">
+			<div id="/section-9/page-1" class="ft-page page-52 full-img" data-id="page-1">
+				<h3 id="text-71" contenteditable="true">愿得一人心，白首不相离</h3>
+				<img src='img/iali20.jpg'  />
+			</div>
+			<div id="/section-9/page-2" class="ft-page page-53" data-id="page-2">
+				<img src='img/iali68.jpg' alt='happy birthday' />
+				<h3 id="text-72" contenteditable="true">英子，情人节快乐</h3>
+			</div>
+			<div id="/section-9/page-3" class="ft-page page-54 center-img" data-id="page-3" >
+				<div class="center-img">I love you</div>
+			</div>
+			<div id="/section-9/page-4" class="ft-page page-55 right-img" data-id="page-4">
+				<img src='img/14915.jpg' />
+				<p class="text">
+					<span id="text-73" contenteditable="true">以后的开心喜悦，我都愿意陪着你 </span><br />
+					<span id="text-74" contenteditable="true">你就是我喜欢的完美定义的英子呀</span><br />
+					<br />
+					<br />
+					<span> -- 按“Esc"键有惊喜</span>
+				</p>				
+			</div>
+		</div>
+	</div>
+	<div class="nojavascript">您的浏览器版本太低或禁用了javascript，无法正常浏览本页面</div>
+
+
+	<audio id="bgmMusic" src="music/saveme.mp3" autoplay="autoplay" loop preload="auto" type="audio/mp3"></audio>
+    
+	<script src="http://libs.baidu.com/jquery/1.8.3/jquery.min.js"></script>
+	<script src="js/all.min.js"></script>
+	<script src="js/love.min.js"></script>
+    <div class="mPower"><span id="on" title="点击暂停"></span><span id="off" title="点击播放"></span></div>
+</body>
+</html>
